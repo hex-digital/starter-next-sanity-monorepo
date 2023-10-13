f7ef312b9d5352903db136ade1f42771264d3e0b
Jamie Warburton <jamie@hexdigital.com>
2023-10-13 10:35:27 +0100

    feat: remove old references

--- apps/studio/config/index.ts
+++ apps/studio/config/index.ts
@@ -18,7 +18,7 @@ export const apiVersion =
   process.env.SANITY_STUDIO_SANITY_API_VERSION || '2023-09-28';
 
 export const title =
-  process.env.SANITY_STUDIO_SANITY_PROJECT_TITLE || 'BirdLife Datazone Test';
+  process.env.SANITY_STUDIO_SANITY_PROJECT_TITLE || 'Sanity';
 
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
