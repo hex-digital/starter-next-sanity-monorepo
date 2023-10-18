34305bde7a2a229eac6bd56c0db5c0ae9c360f99
Jamie Warburton <jamie@hexdigital.com>
2023-10-18 17:20:36 +0100

    feat: improve readability of index page

--- apps/web/src/app/(site)/layout.tsx
+++ apps/web/src/app/(site)/layout.tsx
@@ -1,18 +1,18 @@
 import { Suspense } from 'react';
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
+import { draftMode } from 'next/headers';
 import { Inter } from 'next/font/google';
 import { TailwindIndicator } from '@packages/ui';
 import { siteConfig } from '@packages/config/site';
 import { token } from "~/sanity/lib/sanityFetch";
 
 import '@packages/ui/styles/globals.css';
-import { draftMode } from 'next/headers';
+
+const inter = Inter({ subsets: ['latin'] });
 
 const PreviewProvider = dynamic(() => import('../_components/Preview/PreviewProvider'))
 const PreviewBar = dynamic(() => import('../_components/Preview/PreviewBar/PreviewBar'));
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: siteConfig.title,
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const isDraftMode = draftMode().isEnabled;
-  const preview = isDraftMode && token;
+  const preview = !!(token && isDraftMode);
 
   const content = (
     <>
@@ -43,7 +43,9 @@ export default function RootLayout({
         {preview
           ? (
             <Suspense fallback={content}>
-              <PreviewProvider token={token}>{content}</PreviewProvider>
+              <PreviewProvider token={token}>
+                {content}
+              </PreviewProvider>
             </Suspense>
           )
           : content
